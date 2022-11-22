54d3f0de83fb86df6403c5b7c209de3a5ff5ec9e
thomasyyb <thomas@LAPTOP-5V6K8E71.localdomain>
2022-11-22 11:44:11 -0500

    mouse halfway, camera working with 3 bits

---
+++ mouse/build/CMakeFiles/mouse_ps2_pio_h.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mouse_ps2_pio_h"
+  "ps2.pio.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/mouse_ps2_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
